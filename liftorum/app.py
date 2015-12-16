2a7feaf2713928d3f62bc303f867c2da12c9ebee
Voxy <voxy@massover.local>
2015-12-15 22:05:52 -0500

    Serialize url field on video model

--- liftorum/app.py
+++ liftorum/app.py
@@ -6,7 +6,7 @@ from .extensions import db, bootstrap, mail, migrate, s3, api_manager, jwt
 from flask_restless import ProcessingException
 from flask.ext.security import current_user
 from flask.ext.security.utils import verify_password
-from flask_jwt import current_identity
+from flask_jwt import current_identity, jwt_required
 
 def create_app():
     app = Flask(__name__)
@@ -34,30 +34,33 @@ def create_app():
     s3.init_app(app)
 
     @jwt.authentication_handler
-    def authenticate(email, password):
+    def authentication_handler(email, password):
         user = user_datastore.find_user(email=email.lower())
         if user and verify_password(password, user.password):
             return user
         return None
 
     @jwt.identity_handler
-    def identity(payload):
+    def identity_handler(payload):
         user = user_datastore.find_user(id=payload['identity'])
         return user
 
     @jwt.auth_response_handler
-    def auth_response(access_token, identity):
+    def auth_response_handler(access_token, identity):
         return jsonify({
             'access_token': access_token.decode('utf-8'),
             'user_id': identity.id
         })
 
+    @jwt.jwt_error_handler
+    def error_handler(e):
+        raise ProcessingException(description=str(e), code=401)
+
     jwt.init_app(app)
 
-    def authentication_preprocessor(*args, **kw):
+    @jwt_required()
+    def authentication_preprocessor(**kw):
         pass
-        #if not current_user.is_authenticated():
-        #    raise ProcessingException(description='Not authenticated!', code=401)
 
     def post_preprocessor(data=None, **kw):
         #data['user_id'] = current_user.id
@@ -69,7 +72,7 @@ def create_app():
     api_manager.init_app(
         app,
         flask_sqlalchemy_db=db,
-        preprocessors={'POST': [authentication_preprocessor]},
+        preprocessors=[authentication_preprocessor],
     )
     from liftorum.main.models import Lift, Video, Comment
     api_manager.create_api(
